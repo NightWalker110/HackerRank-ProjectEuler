f18387a892b01e22e5fe0122c8d893a735707f89
Shubham Kumar Singh <61842432+NightWalker110@users.noreply.github.com>
2020-12-30 15:38:32 +0530

    Create Project Euler #2: Even Fibonacci numbers.java

---
+++ Project Euler #2: Even Fibonacci numbers.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            long n = in.nextLong();
+        }
+    }
+}
